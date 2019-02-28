50072ce8f1f4dc3c76833922939abe1f266b4c8b
Serhii Radkevych <sr@devurai.com>
2019-02-28 17:44:53 +0200

    remove storage token and redirect to login if 401 Unauthorised

--- src/app/utils/interceptors/storage-token.interceptor.ts
+++ src/app/utils/interceptors/storage-token.interceptor.ts
@@ -1,7 +1,17 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Inject, InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs';
-import { KEY } from '../../services/local-storage';
+import {
+    HttpErrorResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+    HttpResponse
+} from '@angular/common/http';
+import {Inject, Injectable, InjectionToken} from '@angular/core';
+import {Observable} from 'rxjs';
+import {KEY, LocalStorageService} from '../../services/local-storage';
+import {Router} from "@angular/router";
+import {tap} from 'rxjs/operators';
+import {AuthServiceConfig, AUTH_SERVICE_CONFIG} from "../../services/auth";
 
 export const HTTP_STORAGE_TOKEN_KEY = new InjectionToken<string>('HTTP_STORAGE_TOKEN_KEY');
 
@@ -10,7 +20,12 @@ export class StorageTokenInterceptor implements HttpInterceptor {
         return JSON.parse(localStorage.getItem(`${KEY}: ${this.tokenKey}`));
     }
 
-    constructor(@Inject(HTTP_STORAGE_TOKEN_KEY) private readonly tokenKey: string) {}
+    constructor(
+        @Inject(HTTP_STORAGE_TOKEN_KEY) private readonly tokenKey: string,
+        @Inject(AUTH_SERVICE_CONFIG) private readonly config: AuthServiceConfig,
+        private readonly localStoreService: LocalStorageService,
+        private readonly router: Router) {
+    }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (this.token) {
@@ -20,6 +35,19 @@ export class StorageTokenInterceptor implements HttpInterceptor {
                 }
             });
         }
-        return next.handle(request);
+        return next.handle(request).pipe(
+            tap((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                }
+            }, (err: any) => {
+                if (err instanceof HttpErrorResponse) {
+                    if (err.status === 401) {
+                        this.localStoreService.removeItem(this.config.storageTokenKey);
+                        this.localStoreService.removeItem(this.config.user);
+                        this.router.navigate(['login']);
+                    }
+                }
+            })
+        );
     }
 }
