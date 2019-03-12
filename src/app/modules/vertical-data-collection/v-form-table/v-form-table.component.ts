3a9b17bc9e900536489a0ab6bf21ae3cba4f7447
Serhii Radkevych <sr@devurai.com>
2019-03-12 19:13:52 +0200

    add base layout and css to form builder list

--- src/app/modules/vertical-data-collection/v-form-table/v-form-table.component.ts
+++ src/app/modules/vertical-data-collection/v-form-table/v-form-table.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Form} from "../model/form.model";
 import {VFormService} from "../v-form.service";
 
 @Component({
   selector: 'app-v-form-table',
   templateUrl: './v-form-table.component.html',
-  styleUrls: ['./v-form-table.component.css']
+  styleUrls: ['./v-form-table.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class VFormTableComponent implements OnInit {
 
