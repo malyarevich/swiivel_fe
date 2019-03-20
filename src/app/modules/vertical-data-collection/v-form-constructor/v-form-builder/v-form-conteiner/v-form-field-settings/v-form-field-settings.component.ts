69a9ed8708c9fd98e9a9fb2d79cfd82e15eb11ac
Serhii Radkevych <sr@devurai.com>
2019-03-20 17:49:26 +0200

    RED 61 add input field size and mask

--- src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-form-conteiner/v-form-field-settings/v-form-field-settings.component.ts
+++ src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-form-conteiner/v-form-field-settings/v-form-field-settings.component.ts
@@ -11,16 +11,18 @@ export class VFormFieldSettingsComponent implements OnInit {
     tab: string = 'settings';
 
     validationRules = [
-        {name: 'Alphabetic', rule: 'alpha', min: true, max: true},
-        {name: 'Alpha Numeric', rule: 'alpha_num', min: true, max: true},
-        {name: 'Alpha Numeric Dash', rule: 'alpha_dash', min: true, max: true},
-        {name: 'Data', rule: 'data', min: false, max: false},
-        {name: 'Email', rule: 'email', min: false, max: false},
-        {name: 'Numeric', rule: 'numeric', min: true, max: true},
-        {name: 'String', rule: 'string', min: true, max: true},
-        {name: 'URL', rule: 'url', min: false, max: false},
+        {name: 'Alphabetic', rule: 'alpha', min: true, max: true, param: false},
+        {name: 'Alpha Numeric', rule: 'alpha_num', min: true, max: true, param: false},
+        {name: 'Alpha Numeric Dash', rule: 'alpha_dash', min: true, max: true, param: false},
+        {name: 'Data', rule: 'data', min: false, max: false, param: false},
+        {name: 'Email', rule: 'email', min: false, max: false, param: false},
+        {name: 'Numeric', rule: 'numeric', min: true, max: true, param: false},
+        {name: 'String', rule: 'string', min: true, max: true, param: false},
+        {name: 'URL', rule: 'url', min: false, max: false, param: false},
+        {name: 'IN', rule: 'in',  min: false, max: false, param: true}
     ];
 
+    ruleDefault = {name: '', rule: '', min: false, max: false, param: false};
     rule: object;
     field;
 
@@ -29,7 +31,7 @@ export class VFormFieldSettingsComponent implements OnInit {
 
     ngOnInit() {
         this.field = this.inputField;
-        this.rule = this.field.validators[0];
+        this.rule = this.field.validators[0] ? this.field.validators[0] : this.ruleDefault;
     }
 
     isValidatorChanged(event) {
@@ -37,13 +39,12 @@ export class VFormFieldSettingsComponent implements OnInit {
         this.changeFieldEmitter.emit(this.field);
     }
 
-    addValidatorRule(rule) {
+    changeValidatorRule(rule) {
         this.rule = this.validationRules.find(elem => elem.rule == rule);
-        this.field.validators = [this.rule];
-        this.changeFieldEmitter.emit(this.field);
+        this.addValidatorRule();
     }
 
-    addMinMaxRule() {
+    addValidatorRule() {
         this.field.validators = [this.rule];
         this.changeFieldEmitter.emit(this.field);
     }
@@ -60,6 +61,21 @@ export class VFormFieldSettingsComponent implements OnInit {
 
     addErrorText(event) {
         this.field.options.errorText = event;
+        this.changeFieldEmitter.emit(this.field);
+    }
+
+    changeMinFieldSize(event) {
+        this.field.options.minFieldSize = event;
+        this.changeFieldEmitter.emit(this.field);
+    }
+
+    changeMaxFieldSize(event) {
+        this.field.options.maxFieldSize = event;
+        this.changeFieldEmitter.emit(this.field);
+    }
+
+    changeInputMask(event) {
+        this.field.options.inputMask = event;
         this.changeFieldEmitter.emit(this.field);
     }
 }
