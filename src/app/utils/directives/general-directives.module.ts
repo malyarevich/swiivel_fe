41d9964ff6abe805aac22cbb36a0f4e99cace229
Serhii Radkevych <sr@devurai.com>
2019-04-08 17:47:30 +0300

    RED-114 add custom min max validation

--- src/app/utils/directives/general-directives.module.ts
+++ src/app/utils/directives/general-directives.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {ClickStopPropagationDirective} from "./click-stop-propagation.directive";
+import {CustomMaxDirective} from "./custom-max-validator.directive";
+import {CustomMinDirective} from "./custom-min-validator.directive";
 
 @NgModule({
-    declarations: [ClickStopPropagationDirective],
-    exports: [ClickStopPropagationDirective],
+    declarations: [ClickStopPropagationDirective, CustomMaxDirective, CustomMinDirective],
+    exports: [ClickStopPropagationDirective, CustomMaxDirective, CustomMinDirective],
 })
 export class GeneralDirectivesModule {
 }
