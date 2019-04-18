80bc486db9f4969123488dcdfb97a6fbf82c5b3e
Serhii Radkevych <sr@devurai.com>
2019-04-18 17:00:07 +0300

    RED-138 add layout form info stats

--- src/app/modules/vertical-data-collection/v-form-info/v-form-info-stats/v-form-info-stats.component.ts
+++ src/app/modules/vertical-data-collection/v-form-info/v-form-info-stats/v-form-info-stats.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {FormSql} from "../../model/form.model";
+import {FormsViewsRate} from "../../model/forms-stats.models";
+import {FormsStatsService} from "../../services/forms-stats.service";
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-v-form-info-stats',
   templateUrl: './v-form-info-stats.component.html',
-  styleUrls: ['./v-form-info-stats.component.css']
+  styleUrls: ['./v-form-info-stats.component.scss']
 })
-export class VFormInfoStatsComponent implements OnInit {
+export class VFormInfoStatsComponent implements OnInit, AfterViewInit {
+  @Input() form: FormSql;
 
-  constructor() { }
+  @ViewChild('chartTotalViews') chartTotalViews: ElementRef;
+  @ViewChild('chartMonthViews') chartMonthViews: ElementRef;
 
-  ngOnInit() {
+  chartTotal: Chart = {};
+  chartMonth: Chart = {};
+
+  viewsRate: FormsViewsRate;
+
+  constructor(private formsStatsService: FormsStatsService) {
   }
 
+  ngOnInit() {
+    // this.formsStatsService.getViewsRate().subscribe((val: FormsViewsRate) => {
+    //   this.viewsRate = val;
+    // }
+    this.viewsRate = { total: 867, month: 135, today: 12};
+  }
+
+  ngAfterViewInit() {
+    this.chartTotalViewsInit();
+    this.chartLastMonthInit();
+  }
+
+  chartTotalViewsInit() {
+    this.chartTotal = new Chart(this.chartTotalViews.nativeElement.getContext('2d'), {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [27, 73],
+          backgroundColor: ['#F5A623', '#E6E6E6'],
+          borderWidth: 1,
+        }],
+
+      },
+      options: {
+        responsive: true,
+        cutoutPercentage: 90,
+      }
+    });
+  }
+
+  chartLastMonthInit() {
+    this.chartMonth = new Chart(this.chartMonthViews.nativeElement.getContext('2d'), {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [34, 66],
+          backgroundColor: ['#4051A8', '#E6E6E6'],
+          borderWidth: 1,
+        }],
+
+      },
+      options: {
+        responsive: true,
+        cutoutPercentage: 90,
+      }
+    });
+  }
 }
