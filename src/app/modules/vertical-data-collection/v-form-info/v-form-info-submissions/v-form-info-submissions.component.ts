33744e2f036c22f8e070783f89246b66d41cce62
Serhii Radkevych <sr@devurai.com>
2019-04-22 10:37:01 +0300

    RED-140 add show hide row info to submission form info

--- src/app/modules/vertical-data-collection/v-form-info/v-form-info-submissions/v-form-info-submissions.component.ts
+++ src/app/modules/vertical-data-collection/v-form-info/v-form-info-submissions/v-form-info-submissions.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VFormInfoSubmissionsComponent implements OnInit {
 
+  showRowInfo: boolean = true;
+
   cols = [
     {
       id: 'account',
