7575160276afcc75f5418231aa0590f3a5836c42
Serhii Radkevych <sr@devurai.com>
2019-04-23 13:08:08 +0300

    RED-151 split general setting by sections

--- src/app/modules/vertical-data-collection/v-form-constructor/v-form-general-information/v-form-general-menu/v-form-general-menu.component.ts
+++ src/app/modules/vertical-data-collection/v-form-constructor/v-form-general-information/v-form-general-menu/v-form-general-menu.component.ts
@@ -1,53 +1,61 @@
 import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    EventEmitter,
-    HostListener,
-    OnInit,
-    Output,
-    ViewChild
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+  ViewChild
 } from '@angular/core';
 
 @Component({
-    selector: 'app-v-form-general-menu',
-    templateUrl: './v-form-general-menu.component.html',
-    styleUrls: ['./v-form-general-menu.component.css']
+  selector: 'app-v-form-general-menu',
+  templateUrl: './v-form-general-menu.component.html',
+  styleUrls: ['./v-form-general-menu.component.css']
 })
-export class VFormGeneralMenuComponent implements OnInit, AfterViewInit {
+export class VFormGeneralMenuComponent implements OnInit {
 
-    @ViewChild('generalInfoMenu') menuElement: ElementRef;
-    @Output() scrollToEmitter = new EventEmitter<string>();
+  // @ViewChild('generalInfoMenu') menuElement: ElementRef;
+  // @Output() scrollToEmitter = new EventEmitter<string>();
+  @Output() activeSectionEmitter = new EventEmitter<string>();
 
-    sticky: boolean = false;
-    elementPosition: any;
-    activeItem: string;
+  // sticky: boolean = false;
+  // elementPosition: any;
+  activeItem: number = 1;
 
-    menuItems = [
-        {title: 'Basic Form Information', target: 'basicInfo'},
-        {title: 'Period', target: 'period'},
-        {title: 'Form Dates', target: 'formDates'},
-        {title: 'Eligible Accounts', target: 'eligibleAccounts'},
-    ];
+  menuItems = [
+    {title: 'Basic Form Information', target: 1},
+    {title: 'Period', target: 2},
+    {title: 'Form Dates', target: 3},
+    {title: 'Eligible Accounts', target: 4},
+  ];
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    ngAfterViewInit() {
-        this.elementPosition = this.menuElement.nativeElement.offsetParent.offsetTop;
-    }
+  // ngAfterViewInit() {
+  //     this.elementPosition = this.menuElement.nativeElement.offsetParent.offsetTop;
+  // }
 
-    @HostListener('window:scroll', ['$event'])
-    handleScroll() {
-        const windowScroll = window.pageYOffset;
-        this.sticky = (windowScroll >= this.elementPosition);
-    }
+  // @HostListener('window:scroll', ['$event'])
+  // handleScroll() {
+  //     const windowScroll = window.pageYOffset;
+  //     this.sticky = (windowScroll >= this.elementPosition);
+  // }
 
-    onScrollTo(target) {
-        this.activeItem = target;
-        this.scrollToEmitter.emit(target);
-    }
+  // onScrollTo(target) {
+  //     this.activeItem = target;
+  //     this.scrollToEmitter.emit(target);
+  // }
+
+  onSetSection(value) {
+    this.activeItem = value;
+    this.activeSectionEmitter.emit(value);
+  }
+
+
 }
