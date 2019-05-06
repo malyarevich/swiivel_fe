57ea891b1489e706cd6b68aed9c138b48ae12dfc
Serhii Radkevych <sr@devurai.com>
2019-05-06 13:02:34 +0300

    RED-182 init workplace terms and conditions to FB

--- src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-terms-conditions/v-terms-conditions.component.ts
+++ src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-terms-conditions/v-terms-conditions.component.ts
@@ -1,18 +1,28 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ConsentItemInfo} from "../v-consent/model/consent.model";
-import {TermsConditionsItem} from "./model/terms-conditions.model";
+import {TermsConditions} from "./model/terms-conditions.model";
+import {E_SIGNATURE_TYPES, SIGNATURE_TYPES} from "../../../../../enums";
 
 @Component({
   selector: 'app-v-terms-conditions',
   templateUrl: './v-terms-conditions.component.html',
-  styleUrls: ['./v-terms-conditions.component.css']
+  styleUrls: ['./v-terms-conditions.component.scss']
 })
 export class VTermsConditionsComponent implements OnInit {
-  @Input() termsConditions: TermsConditionsItem[];
+  @Input() termsConditions: TermsConditions;
+
+  sectionName: string = 'Terms and Conditions';
+
+  SIGNATURE_TYPES = SIGNATURE_TYPES;
+  E_SIGNATURE_TYPES = E_SIGNATURE_TYPES;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setCommand(name: string, showUi: boolean = false, value: string = null) {
+    document.execCommand(name, showUi, value);
   }
 
 }
