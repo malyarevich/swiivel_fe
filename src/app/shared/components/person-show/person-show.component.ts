5978e2ee81ffced0bd55d0768181fe7d46fa6046
Artem Kuzvesov <arktuz@gmail.com>
2019-05-09 19:41:26 +0500

    Payer view

---
+++ src/app/shared/components/person-show/person-show.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-person-show',
+  templateUrl: './person-show.component.html',
+  styleUrls: ['./person-show.component.scss']
+})
+export class PersonShowComponent implements OnInit {
+  @Input() avatar: string;
+  @Input() name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
