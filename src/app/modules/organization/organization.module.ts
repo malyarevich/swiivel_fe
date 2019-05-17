91b95a9dee5dd246a6f48f2e9c9a0cece9f443ca
Paul Malyarevich <p@codeblue.ventures>
2019-05-17 18:14:26 +0300

    RED-199 Mobile first - Main Menu + Top Menu (layout refactoring)

--- src/app/modules/organization/organization.module.ts
+++ src/app/modules/organization/organization.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {OrganisationRoutingModule} from "./organisation-routing.module";
 import {LayoutComponent} from "./components/layout";
 import {HeaderComponent} from "./components/header";
+import {HeaderTopComponent} from "./components/header-top";
 import {StoreModule} from "@ngrx/store";
 import {EffectsModule} from "@ngrx/effects";
 import {PersonService} from "./modules/person/services/person.service";
@@ -17,6 +18,7 @@ import {FamilyService} from "../../services/family/family.service";
     declarations: [
         LayoutComponent,
         HeaderComponent,
+        HeaderTopComponent,
     ],
     imports: [
         CommonModule,
