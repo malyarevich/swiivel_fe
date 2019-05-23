1b7820d1d262bfde2c0d8a87563d41e5875dea6b
Serhii Radkevych <sr@devurai.com>
2019-05-23 16:18:04 +0300

    fix family name to family general info

--- src/app/modules/family/family-profile/family-view/family-view-general/family-view-general-nav/family-view-general-nav.component.ts
+++ src/app/modules/family/family-profile/family-view/family-view-general/family-view-general-nav/family-view-general-nav.component.ts
@@ -1,13 +1,19 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FamilyViewGeneralTab, familyViewGeneralTabs,} from '../../../models/family-view-general-tabs'
+import {Family} from "../../../../../../models/family/family.model";
+import {FamilyService} from "../../../../services/family.service";
+
 @Component({
   selector: 'app-family-view-general-nav',
   templateUrl: './family-view-general-nav.component.html',
   styleUrls: ['./family-view-general-nav.component.css']
 })
 export class FamilyViewGeneralNavComponent implements OnInit {
+  @Input() familyId;
   @Output() activeTabEmitter = new EventEmitter<string>();
   @Output() searchQueryEmitter = new EventEmitter<string>();
+
+  familyName: string;
 
   familyViewGeneralTabs: FamilyViewGeneralTab[] = familyViewGeneralTabs;
   activeTab: string;
@@ -15,10 +21,17 @@ export class FamilyViewGeneralNavComponent implements OnInit {
 
   showModal: boolean = false;
 
-  constructor() { }
+  constructor(private familyService: FamilyService) { }
 
   ngOnInit() {
+    this.getFamilyName();
     this.setActiveTab(this.familyViewGeneralTabs[0].name);
+  }
+
+  getFamilyName() {
+    this.familyService.family.subscribe((res: Family) => {
+      this.familyName = res.name;
+    })
   }
 
   setActiveTab(tabName) {
