5da35701ce62fe812aa42164b4ad227add4e4e0b
Paul Malyarevich <p@codeblue.ventures>
2019-05-27 03:40:07 +0300

    RED-240 Data collections hidding the widgets

--- src/app/modules/vertical-data-collection/v-form-table/v-form-statistic/v-form-statistic.component.ts
+++ src/app/modules/vertical-data-collection/v-form-table/v-form-statistic/v-form-statistic.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-v-form-statistic',
   templateUrl: './v-form-statistic.component.html',
-  styleUrls: ['./v-form-statistic.component.css']
+  styleUrls: ['./v-form-statistic.component.scss']
 })
 export class VFormStatisticComponent implements OnInit {
+  private isShowWidgets: boolean = false;
+  private labelHideWidget: string = "hide the widgets";
+  private labelShowWidget: string = "view widgets";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleWidgets(): void {
+    this.isShowWidgets = !this.isShowWidgets;
+  }
+  
 }
