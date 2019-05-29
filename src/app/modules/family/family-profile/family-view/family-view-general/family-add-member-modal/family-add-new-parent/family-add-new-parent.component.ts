33c7d76fbe9cf692d4e8c6d1191a5df620f29192
Serhii Radkevych <sr@devurai.com>
2019-05-29 13:18:13 +0300

    RED-242 add create parents to family view

--- src/app/modules/family/family-profile/family-view/family-view-general/family-add-member-modal/family-add-new-parent/family-add-new-parent.component.ts
+++ src/app/modules/family/family-profile/family-view/family-view-general/family-add-member-modal/family-add-new-parent/family-add-new-parent.component.ts
@@ -1,7 +1,10 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Family} from "../../../../../../../models/family/family.model";
 import {FamilyPersonService} from "../../../../../../../services/family/family-person.service";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FamilyRoles} from "../../../../../../../enums/family-roles";
+import {PersonService} from "../../../../../../../services/person/person.service";
+import {Gender} from "../../../../../../../enums/gender";
 
 @Component({
   selector: 'app-family-add-new-parent',
@@ -13,9 +16,12 @@ export class FamilyAddNewParentComponent implements OnInit {
   @Input() family: Family;
   @Input() role: string;
 
+  familyRoles = FamilyRoles;
+  gender = Gender;
   familyParentForm: FormGroup;
 
   constructor(private familyPersonService: FamilyPersonService,
+              private personService: PersonService,
               private fb: FormBuilder,) {
     this.initFamilyNewPersonForm();
   }
@@ -25,16 +31,56 @@ export class FamilyAddNewParentComponent implements OnInit {
 
   initFamilyNewPersonForm() {
     this.familyParentForm = this.fb.group({
-      // role: [this.roles[0], Validators.required],
+      first_name: [null, Validators.required],
+      middle_name: [null],
+      last_name: [null],
+      dob: [null],
+      full_hebrew_name: [null],
+      maiden_name: [null],
+      occupation: [null],
+      address: [null],
+      corporate_phone: [null],
+      contact_phone: [null],
+      contact_email: [null],
     });
   }
 
   addFamilyParent() {
-    console.log('sdsd');
+    if (this.familyParentForm.invalid) return;
+    let data = {
+      ...this.familyParentForm.value,
+      gender: this.getGenderByRole(this.role),
+    };
+    this.personService.addPerson(data).subscribe((res) => {
+      if (res.created) {
+        this.addFamilyPerson(res.data.id);
+      }
+    }, (error) => console.log(error));
     this.onCloseAddFamilyMemberModal();
+  }
+
+  addFamilyPerson(personId) {
+    const data = {
+      family_id: this.family.family_id,
+      family_name: this.family.name,
+      id_person: personId,
+      person_role: this.role,
+    };
+    this.familyPersonService.add(data);
   }
 
   onCloseAddFamilyMemberModal() {
     this.closeModalAddFamilyMember.emit(true);
+  }
+
+  getGenderByRole(role) {
+    switch (role) {
+      case this.familyRoles.father:
+        return this.gender.MALE;
+      case this.familyRoles.mother:
+        return this.gender.FEMALE;
+      default:
+        return null;
+    }
   }
 }
