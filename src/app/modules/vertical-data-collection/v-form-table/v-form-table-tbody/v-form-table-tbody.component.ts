8d7d835ea68f4d02cf3d69b93d87d5735ea86a18
Paul Malyarevich <p@codeblue.ventures>
2019-06-12 18:56:17 +0300

    v-form-table bulk fix and rework

--- src/app/modules/vertical-data-collection/v-form-table/v-form-table-tbody/v-form-table-tbody.component.ts
+++ src/app/modules/vertical-data-collection/v-form-table/v-form-table-tbody/v-form-table-tbody.component.ts
@@ -1,28 +1,79 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormSql } from "../../../data-collection/reducers/forms/form.model";
+import { VFormService } from "../../services/v-form.service";
+import { TEMPLATE_STATUS } from "../../../../enums/template-status";
 
 @Component({
-    selector: '[app-v-form-table-tbody]',
-    templateUrl: './v-form-table-tbody.component.html',
-    styleUrls: ['./v-form-table-tbody.component.scss']
+  selector: "[app-v-form-table-tbody]",
+  templateUrl: "./v-form-table-tbody.component.html",
+  styleUrls: ["./v-form-table-tbody.component.scss"]
 })
 export class VFormTableTbodyComponent implements OnInit {
-    @Input() parent: any;
+  @Input() forms: FormSql[];
+  @Input() formSelected: any;
+  @Input() templateStatuses: any;
+  @Input() changedStatuses: any;
 
-    constructor() { }
+  @Output() addSelectedIdsEmitter: any = new EventEmitter();
+  @Output() setFormSelectedEmitter: any = new EventEmitter();
+  @Output() resetFormSelectedEmitter: any = new EventEmitter();
+  @Output() changeStatusEmitter: any = new EventEmitter();
+  @Output() getAllFormEmitter: any = new EventEmitter();
 
-    ngOnInit() { }
+  checkedRows: Array<any> = [];
+  constructor(private vFormService: VFormService) {}
 
-    getStringPublishSettings(publish_settings: any) {
-        if (publish_settings == undefined) {
-            return '';
-        }
-        return publish_settings.state.settings.online === false 
-            ? publish_settings.state.settings.pdf === false 
-                ? ''
-                : 'Paper'
-            : publish_settings.state.settings.pdf === false
-                ? 'Online'
-                : 'Online, Paper';
+  ngOnInit() {}
+
+  getStringPublishSettings(publish_settings: any) {
+    if (publish_settings == undefined) {
+      return "";
     }
-    
+    return publish_settings.state.settings.online === false
+      ? publish_settings.state.settings.pdf === false
+        ? ""
+        : "Paper"
+      : publish_settings.state.settings.pdf === false
+      ? "Online"
+      : "Online, Paper";
+  }
+
+  // TODO: Create directive ConvertData for Safari browser
+  convertDate(date) {
+    return date.replace(/\s/g, "T");
+  }
+
+  getClassByStatus(status) {
+    return {
+      'app-btn-status-active': status === TEMPLATE_STATUS.STATUS_ACTIVE,
+      'app-btn-status-archived': status === TEMPLATE_STATUS.STATUS_ARCHIVED,
+      'app-btn-status-draft': status === TEMPLATE_STATUS.STATUS_DRAFT,
+      'app-btn-status-review': status === TEMPLATE_STATUS.STATUS_REVIEW,
+      'app-btn-status-closed': status === TEMPLATE_STATUS.STATUS_CLOSED,
+    }
+  }
+
+  addSelectedIds(id: any) {
+    this.addSelectedIdsEmitter.emit(id);
+    this.checkedRows[id] = !this.checkedRows[id];
+  }
+
+  isCheckedRow(id: any) {
+    return this.checkedRows.find(id);
+  }
+
+  setFormSelected(id: any) {
+    this.setFormSelectedEmitter.emit(id);
+  }
+
+  resetFormSelected() {
+    this.resetFormSelectedEmitter.emit();
+    this.checkedRows = [];
+  }
+  
+  changeStatus(id, status) {
+    this.vFormService.changeStatus(id, status).subscribe(res => {
+      this.getAllFormEmitter.emit();
+    });
+  }
 }
