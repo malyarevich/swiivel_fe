5d3e528f08702366feb843b2c1704eb9d3abc4e9
Serhii Radkevych <sr@devurai.com>
2019-07-17 16:44:56 +0300

    add reactive form to fields conditional logic

--- src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-form-fields/v-fields-workspace/v-fields-shared-components/v-fields-conditional-logic/v-fields-conditional-logic.component.ts
+++ src/app/modules/vertical-data-collection/v-form-constructor/v-form-builder/v-form-fields/v-fields-workspace/v-fields-shared-components/v-fields-conditional-logic/v-fields-conditional-logic.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Field, FieldSettingConditional} from "../../../../../../../../models/vertical-data-collection/field.model";
+import {cloneDeep} from 'lodash';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+const defaultConditional: FieldSettingConditional = {
+  logic: 'hide',
+  when: 'all',
+  field: '',
+  rule: 'is',
+  value: '',
+};
 
 @Component({
   selector: 'app-v-fields-conditional-logic',
@@ -6,12 +17,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./v-fields-conditional-logic.component.css']
 })
 export class VFieldsConditionalLogicComponent implements OnInit {
+  @Input() inputField: Field;
+  conditionalForm: FormGroup;
 
-  conditLogic = ['Hide', 'Visible'];
+  conditLogic = ['hide', 'visible'];
 
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
+    this.initConditional();
+    this.initConditionalForm();
+    this.onChangesConditionalForm();
   }
+
+  initConditional() {
+    if (!this.inputField.conditional) {
+      this.inputField['conditional'] = Object.assign(defaultConditional);
+    }
+  }
+
+  initConditionalForm() {
+    this.conditionalForm = this.fb.group({
+      logic: [this.inputField.conditional.logic],
+      when: [this.inputField.conditional.when],
+      field: [this.inputField.conditional.field],
+      rule: [this.inputField.conditional.rule],
+      value: [this.inputField.conditional.value]
+    });
+  }
+
+  onChangesConditionalForm() {
+    this.conditionalForm.valueChanges.subscribe((val) => {
+      this.inputField.conditional = Object.assign(val);
+    });
+  }
+
 
 }
