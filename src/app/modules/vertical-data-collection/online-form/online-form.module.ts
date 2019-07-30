a19eafe902d2b69a190d25aec1bb8b5d6b87ffb8
Vlad Zlenko <vz@devurai.com>
2019-07-30 19:17:39 +0300

    fixed online-form

--- src/app/modules/vertical-data-collection/online-form/online-form.module.ts
+++ src/app/modules/vertical-data-collection/online-form/online-form.module.ts
@@ -46,7 +46,7 @@ import { DynamicModule } from 'ng-dynamic-component';
 import { ErrorListComponent } from './online-form-fields/error-list/error-list.component';
 import { OnlineFormNavigationService } from './services/online-form-navigation.service';
 import { GeneralDirectivesModule } from '../../../utils/directives/general-directives.module';
-import {GeneralPipesModule} from '../../../utils/pipes/general-pipes.module';
+import { GeneralPipesModule } from '../../../utils/pipes/general-pipes.module';
 
 @NgModule({
   declarations: [
@@ -107,8 +107,10 @@ import {GeneralPipesModule} from '../../../utils/pipes/general-pipes.module';
       DropDownListFieldComponent,
       DateTimeFieldComponent,
       TimeFieldComponent,
-    ])
+    ]),
+    GeneralDirectivesModule,
+    GeneralPipesModule
   ],
-  providers: [OnlineFormService, SystemSignatureService]
+  providers: [OnlineFormService, OnlineFormNavigationService, SystemSignatureService]
 })
 export class OnlineFormModule {}
