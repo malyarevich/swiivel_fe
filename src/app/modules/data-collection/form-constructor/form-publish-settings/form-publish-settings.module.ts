a213c97b45bdea3854c4e0794295a5cbfd3edc98
Serhii Radkevych <sr@devurai.com>
2019-08-14 16:53:36 +0300

    add automation module to form publish settings

--- src/app/modules/data-collection/form-constructor/form-publish-settings/form-publish-settings.module.ts
+++ src/app/modules/data-collection/form-constructor/form-publish-settings/form-publish-settings.module.ts
@@ -3,10 +3,6 @@ import {CommonModule} from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormPublishSettingsRoutingModule} from './form-publish-settings-routing.module';
 import {FormPublishSettingsComponent} from './form-publish-settings.component';
-import {AutomationComponent} from './automation/automation.component';
-import {AutomationHeaderComponent} from './automation/automation-header/automation-header.component';
-import {AutomationLogicAreaComponent} from './automation/automation-logic-area/automation-logic-area.component';
-import {AutomationTemplateAreaComponent} from './automation/automation-template-area/automation-template-area.component';
 import {RedirectPagesComponent} from './redirect-pages/redirect-pages.component';
 import {PublishMenuComponent} from './publish-menu/publish-menu.component';
 import {PublishMenuComponentAdditionalOptions} from './publish-menu/additional-options/additional-options.component';
@@ -15,17 +11,14 @@ import {PublishSettingsRemoteService} from '../../services/publish-settings-remo
 import {PublishSettingsAutomationService} from '../../services/publish-settings-automation.service';
 import {PublishSettingsPublishSettingsService} from '../../services/publish-settings-publish-settings.service';
 import {SharedRedComponentsModule} from '@app/shared/components/sharedRedComponents.module';
-import {ConditionsReviewModule} from '@modules/data-collection/form-constructor/form-publish-settings/conditions-review/conditions-review.module';
-import {PublishSettingsModule} from '@modules/data-collection/form-constructor/form-publish-settings/publish-settings/publish-settings.module';
+import {ConditionsReviewModule} from './conditions-review/conditions-review.module';
+import {PublishSettingsModule} from './publish-settings/publish-settings.module';
+import {AutomationModule} from './automation/automation.module';
 
 
 @NgModule({
   declarations: [
     FormPublishSettingsComponent,
-    AutomationComponent,
-    AutomationHeaderComponent,
-    AutomationLogicAreaComponent,
-    AutomationTemplateAreaComponent,
     RedirectPagesComponent,
     PublishMenuComponent,
     PublishMenuComponentAdditionalOptions,
@@ -37,6 +30,7 @@ import {PublishSettingsModule} from '@modules/data-collection/form-constructor/f
     SharedRedComponentsModule,
     ConditionsReviewModule,
     PublishSettingsModule,
+    AutomationModule,
   ],
   providers: [
     PublishSettingsService,
