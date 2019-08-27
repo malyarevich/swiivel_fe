e6795945c267c434a5b12095c38ac3aab06b3c0d
Илья <i@devurai.com>
2019-08-27 23:08:13 +0300

    add check on undefined

--- src/app/core/validators.ts
+++ src/app/core/validators.ts
@@ -3,14 +3,14 @@ import { isEmail, isURL, isDecimal } from 'validator';
 
 export function emailValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         return isEmail(value) ? null : { emailValidator: true };
     }
 }
 
 export function alphabeticValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const regexp = new RegExp(/^[a-zA-Z ]*$/, 'gm');
         return regexp.test(value) ? null : { alphabeticValidator: true };
     }
@@ -18,7 +18,7 @@ export function alphabeticValidator(): ValidatorFn {
 
 export function alphanumericValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const regexp = new RegExp(/^[a-zA-Z0-9 ]*$/, 'gm');
         return regexp.test(value) ? null : { alphanumericValidator: true };
     };
@@ -26,7 +26,7 @@ export function alphanumericValidator(): ValidatorFn {
 
 export function numericValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const regexp = new RegExp(/^[0-9]*$/, 'gm');
         return regexp.test(value) ? null : { numericValidator: true };
     }
@@ -34,28 +34,28 @@ export function numericValidator(): ValidatorFn {
 
 export function urlValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         return isURL(value) ? null : { urlValidator: true };
     }
 }
 
 export function minValueValidator(min: number): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const err = {
             minValueValidator: {
                 current: value,
                 min
             }
         }
-        if (isNaN(value) || !isDecimal(value)) return { minValueValidator: 'Invalid number' };
+        if (isNaN(value)) return { minValueValidator: 'Invalid number' };
         return parseFloat(value) < min ? err : null;
     }
 }
 
 export function maxValueValidator(max: number): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const err = {
             maxValueValidator: {
                 current: value,
@@ -63,14 +63,15 @@ export function maxValueValidator(max: number): ValidatorFn {
             }
 
         }
-        if (isNaN(value) || !isDecimal(value)) return { maxValueValidator: 'Invalid number' };
+        console.log(isNaN(value), isDecimal(value))
+        if (isNaN(value)) return { maxValueValidator: 'Invalid number' };
         return parseFloat(value) > max ? err : null;
     }
 }
 
 export function rangeValueValidator(params: { min?: number, max?: number }): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value.trim();
+        const value = c.value && typeof c.value === 'string' ? c.value.trim() : c.value;
         const err = {
             numberValidator: {
                 min: params.min || null,
@@ -78,14 +79,14 @@ export function rangeValueValidator(params: { min?: number, max?: number }): Val
                 current: value
             }
         }
-        if (isNaN(value) || !isDecimal(value)) {
+        if (isNaN(value)) {
             return { rangeValueValidator: 'Invalid number' };
         } else if (!isNaN(params.min) && !isNaN(params.max)) {
-            return parseFloat(value) < params.min || parseFloat(value) > params.max ? err : null;
+            return (parseFloat(value) < params.min || parseFloat(value) > params.max) ? err : null;
         } else if (!isNaN(params.min)) {
-            return params.min > parseFloat(value) ? err : null;
+            return (params.min > parseFloat(value)) ? err : null;
         } else if (!isNaN(params.max)) {
-            return parseFloat(value) > params.max ? err : null;
+            return (parseFloat(value) > params.max) ? err : null;
         } else {
             return null;
         }
