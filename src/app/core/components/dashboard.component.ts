3e0d29009206140b3c18a76117d1081a5d0c8917
Илья <i@devurai.com>
2019-08-27 21:44:26 +0300

    validators trim string

--- src/app/core/components/dashboard.component.ts
+++ src/app/core/components/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FieldService } from '@core/field.service';
 import fields from '@app/shared/fields';
+import * as vs from '../validators';
 
 
 @Component({
@@ -12,9 +13,17 @@ import fields from '@app/shared/fields';
 export class DashboardComponent implements OnInit {
   form: FormGroup;
   fields = fields;
+  testForm: FormGroup;
   constructor(
-    private fs: FieldService
+    private fs: FieldService,
+    private fb: FormBuilder
   ) {
+    this.testForm = this.fb.group({
+      email: ['', [vs.minValueValidator(1)]],
+    })
+    this.testForm.valueChanges.subscribe(v => {
+      console.log(this.testForm);
+    })
     this.form = this.fs.toForm(fields);
   }
 
--- src/app/core/validators.ts
+++ src/app/core/validators.ts
@@ -1,92 +1,91 @@
-import { ValidatorFn, FormControl, ValidationErrors, Validators } from '@angular/forms';
-import { isEmail, isURL } from 'validator';
+import { ValidatorFn, FormControl, ValidationErrors } from '@angular/forms';
+import { isEmail, isURL, isDecimal } from 'validator';
 
 export function emailValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value;
-        return isEmail(value) ? null : { emailValidator: { valid: false } };
+        const value = c.value.trim();
+        return isEmail(value) ? null : { emailValidator: true };
     }
 }
 
 export function alphabeticValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value;
+        const value = c.value.trim();
         const regexp = new RegExp(/^[a-zA-Z ]*$/, 'gm');
-        return regexp.test(value) ? null : { alphabeticValidator: { valid: false } };
+        return regexp.test(value) ? null : { alphabeticValidator: true };
     }
 }
 
 export function alphanumericValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value;
+        const value = c.value.trim();
         const regexp = new RegExp(/^[a-zA-Z0-9 ]*$/, 'gm');
-        return regexp.test(value) ? null : { alphanumericValidator: { valid: false } };
+        return regexp.test(value) ? null : { alphanumericValidator: true };
     };
 }
 
 export function numericValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value;
+        const value = c.value.trim();
         const regexp = new RegExp(/^[0-9]*$/, 'gm');
-        return regexp.test(value) ? null : { numericValidator: { valid: false } };
+        return regexp.test(value) ? null : { numericValidator: true };
     }
 }
 
 export function urlValidator(): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        const value = c.value;
-        return isURL(value) ? null : { urlValidator: { valid: false } };
+        const value = c.value.trim();
+        return isURL(value) ? null : { urlValidator: true };
     }
 }
 
 export function minValueValidator(min: number): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        if (Validators.required(c)) return null;
-        const value = c.value;
+        const value = c.value.trim();
         const err = {
-            current: value,
-            valid: false,
-            min
+            minValueValidator: {
+                current: value,
+                min
+            }
         }
-        if (isNaN(value)) return err;
-        return value < min ? err : null;
+        if (isNaN(value) || !isDecimal(value)) return { minValueValidator: 'Invalid number' };
+        return parseFloat(value) < min ? err : null;
     }
 }
 
 export function maxValueValidator(max: number): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        if (Validators.required(c)) return null;
-        const value = c.value;
+        const value = c.value.trim();
         const err = {
-            current: value,
-            valid: false,
-            max
+            maxValueValidator: {
+                current: value,
+                max
+            }
+
         }
-        if (isNaN(value)) return err;
-        return value > max ? err : null;
+        if (isNaN(value) || !isDecimal(value)) return { maxValueValidator: 'Invalid number' };
+        return parseFloat(value) > max ? err : null;
     }
 }
 
 export function rangeValueValidator(params: { min?: number, max?: number }): ValidatorFn {
     return (c: FormControl): ValidationErrors => {
-        if (Validators.required(c)) return null;
-        const value = c.value;
+        const value = c.value.trim();
         const err = {
             numberValidator: {
-                valid: false,
                 min: params.min || null,
                 max: params.max || null,
                 current: value
             }
         }
-        if (isNaN(value)) {
-            return err;
+        if (isNaN(value) || !isDecimal(value)) {
+            return { rangeValueValidator: 'Invalid number' };
         } else if (!isNaN(params.min) && !isNaN(params.max)) {
-            return params.min > value || value > params.max ? err : null;
+            return parseFloat(value) < params.min || parseFloat(value) > params.max ? err : null;
         } else if (!isNaN(params.min)) {
-            return params.min > value ? err : null;
+            return params.min > parseFloat(value) ? err : null;
         } else if (!isNaN(params.max)) {
-            return value > params.max ? err : null;
+            return parseFloat(value) > params.max ? err : null;
         } else {
             return null;
         }
