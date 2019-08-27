62de8bb191a1c7359029a3a1ba335d1051c2c3df
Илья <i@devurai.com>
2019-08-27 21:51:31 +0300

    delete trash

--- src/app/core/components/dashboard.component.ts
+++ src/app/core/components/dashboard.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { FieldService } from '@core/field.service';
 import fields from '@app/shared/fields';
-import * as vs from '../validators';
 
 
 @Component({
@@ -13,17 +12,9 @@ import * as vs from '../validators';
 export class DashboardComponent implements OnInit {
   form: FormGroup;
   fields = fields;
-  testForm: FormGroup;
   constructor(
-    private fs: FieldService,
-    private fb: FormBuilder
+    private fs: FieldService
   ) {
-    this.testForm = this.fb.group({
-      email: ['', [vs.minValueValidator(1)]],
-    })
-    this.testForm.valueChanges.subscribe(v => {
-      console.log(this.testForm);
-    })
     this.form = this.fs.toForm(fields);
   }
 
