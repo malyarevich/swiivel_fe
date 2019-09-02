f3ca900698492b4eeb6199353c55771d0aa6661c
Vlad Zlenko <vz@devurai.com>
2019-09-02 13:56:22 +0300

    added shared input-toggle component

--- src/app/core/components/dashboard.component.ts
+++ src/app/core/components/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { FieldService } from '@core/field.service';
 import fields from '@app/shared/fields';
 
@@ -18,7 +18,8 @@ export class DashboardComponent implements OnInit {
     private fb: FormBuilder
   ) {
     this.form = this.fb.group({
-      short: new FormControl('short text')
+      short: new FormControl('short text'),
+      toggle: new FormControl(true)
     });
   }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { InputDynamicComponent } from './input-dynamic/input-dynamic.component';
 import { InputTextComponent } from './input-text/input-text.component';
 import { InputNumberComponent } from './input-number/input-number.component';
+import { InputToggleComponent } from './input-toggle/input-toggle.component';
 
 @NgModule({
   imports:      [
@@ -14,12 +15,14 @@ import { InputNumberComponent } from './input-number/input-number.component';
     InputDynamicComponent,
     InputTextComponent,
     InputNumberComponent,
+    InputToggleComponent,
   ],
   exports: [
     CommonModule,
     InputDynamicComponent,
     InputTextComponent,
     InputNumberComponent,
+    InputToggleComponent,
   ]
 })
 export class SharedModule { }
