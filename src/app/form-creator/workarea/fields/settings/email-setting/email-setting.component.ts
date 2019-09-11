fc6701b4f16ca73e52663f47bf68932a97ba1bf1
Илья <i@devurai.com>
2019-09-11 03:39:32 +0300

    create email field settings

--- src/app/form-creator/workarea/fields/settings/email-setting/email-setting.component.ts
+++ src/app/form-creator/workarea/fields/settings/email-setting/email-setting.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { emailValidator } from '@app/core/validators';
 
 @Component({
   selector: 'sw-email-setting',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailSettingComponent implements OnInit {
 
-  constructor() { }
+  showDefaultValue: boolean = false;
+  form: FormGroup;
+  
+  @Input()
+  set settings(obj: any) {
+    if (obj) {
+      if (!!obj['default'] === true) this.showDefaultValue = true;
+      this.form.patchValue({
+        defaultValue: obj['default'] || '',
+        askForConfirm: !!obj['askForConfirm']
+      });
+    }
+  }
+  @Output() fieldSettings = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      defaultValue: new FormControl('', emailValidator()),
+      askForConfirm: new FormControl(false),
+    });
+    this.form.valueChanges.subscribe(v => {
+      this.fieldSettings.emit(v);
+    })
+  }
+
+  get defaultValue() {
+    return this.form.get('defaultValue');
+  }
+
+  get askForConfirm() {
+    return this.form.get('askForConfirm');
   }
 
 }
