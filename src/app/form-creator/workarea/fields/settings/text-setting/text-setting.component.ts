4ce9757464bdbd1cbd642ccd7f0e623b505b6788
Илья <i@devurai.com>
2019-09-11 18:50:30 +0300

    create text field settings component

--- src/app/form-creator/workarea/fields/settings/text-setting/text-setting.component.ts
+++ src/app/form-creator/workarea/fields/settings/text-setting/text-setting.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ɵConsole } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'sw-text-setting',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextSettingComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  validatorsOptions = ['Alphabetic', 'Alphanumeric', 'Url'].map(t => ({ title: t }));
+
+  @Input()
+  set settings(obj: any) {
+    if (obj) {
+      this.form.patchValue(obj);
+    }
+  }
+  @Output() fieldSettings = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      showDefaultValue: new FormControl(false),
+      showValidators: new FormControl(false),
+      allowList: new FormControl(false),
+      defaultValue: new FormControl(null),
+      validators: new FormGroup({
+        min: new FormControl(null),
+        max: new FormControl(null),
+        validator: new FormControl([])
+      })
+    });
+    this.form.valueChanges.subscribe(v => {
+      delete v.showDefaultValue;
+      delete v.showValidators;
+      this.fieldSettings.emit(v);
+    });
   }
-
 }
