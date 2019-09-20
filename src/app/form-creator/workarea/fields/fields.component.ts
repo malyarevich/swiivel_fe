ba302f4cbb380a77fff295327a1a98eaee7e8431
Илья <i@devurai.com>
2019-09-20 11:52:53 +0300

    fix display settings

--- src/app/form-creator/workarea/fields/fields.component.ts
+++ src/app/form-creator/workarea/fields/fields.component.ts
@@ -1,8 +1,10 @@
 import { ArrayDataSource } from '@angular/cdk/collections';
 import {FlatTreeControl} from '@angular/cdk/tree';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FieldService } from '@app/core/field.service';
 import { fields, sidebar } from '@shared/fields';
+import { ApiService } from '@app/core/api.service';
+import { TreeDataSource } from '@app/form-creator/tree.datasource';
 
 
 @Component({
@@ -12,38 +14,56 @@ import { fields, sidebar } from '@shared/fields';
 })
 export class WorkareaFieldsComponent implements OnInit {
   treeControl = new FlatTreeControl<any>(node => node.level, field => field.type === 113 || field.type === 114);
-  fieldsTree = this.fs.toFlatTree(fields.slice()).reverse();
-  treeSource = new ArrayDataSource(this.fieldsTree);
-  constructor(private fs: FieldService) {
+  sidebar: any[] = [];
+  fieldsTree: any[];
+  treeSource = new TreeDataSource();
 
+  constructor(private fs: FieldService, private api: ApiService, private cdr: ChangeDetectorRef) {
+    this.api.getSidebarFields().subscribe((sidebar) => {
+      this.sidebar = sidebar;
+      this.fieldsTree = this.fs.toFlatTree(this.sidebar.slice());
+      this.fieldsTree.reverse();
+      this.treeSource.data = this.fieldsTree;
+    })
   }
 
   ngOnInit() {
+    // this.filter.valueChanges.subscribe((filter: string) => {
+    //   if (filter && filter.length > 0) {
+    //     this.treeSource.data = this.fieldsTree.filter(field => field.name.includes(filter));
+    //   } else {
+    //     this.treeSource.data = this.fieldsTree;
+    //   }
+    // });
   }
 
   hasChild = (_: number, node: any) => node.expandable;
-
-  getParentNode(node: any) {
-    const nodeIndex = this.fieldsTree.indexOf(node);
-    const parentNode = this.fieldsTree.filter(field => field.type === 113 || field.type === 114).find((field) => {
-      return field.prefix === node.prefix;
-    });
-    // for (let i = nodeIndex - 1; i >= 0; i--) {
-    //   if (this.fieldsTree.level === node.level - 1) {
-    //     return this.fieldsTree;
-    //   }
-    // }
-
-    return parentNode;
-  }
-  shouldRender(node: any) {
-    const parent = this.getParentNode(node);
-    const should = !parent || !parent.expanded;
-    return should;
-  }
 
   getIcon(expanded: boolean): string {
     return expanded ? 'fa-caret-up' : 'fa-caret-down';
   }
 
+
+  getParentNode(node: any) {
+    const nodeIndex = this.fieldsTree.indexOf(node);
+
+    for (let i = nodeIndex - 1; i >= 0; i--) {
+      if (this.fieldsTree[i].level === node.level - 1) {
+        return this.fieldsTree[i];
+      }
+    }
+    return null;
+  }
+  shouldRender(node: any) {
+    const parent = this.getParentNode(node);
+    const should = !parent || parent.expanded;
+    return should;
+  }
+
+  settingsTogle(node: any)  {
+    if (node) {
+      node.showSettings = !node.showSettings;
+      this.cdr.markForCheck();
+    }
+  }
 }
