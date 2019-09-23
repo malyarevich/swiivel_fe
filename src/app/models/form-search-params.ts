f5274d1092cbf3c266bdf54280bc66a788db6fd6
Vitalii Fedorets <vf@codeblue.ventures>
2019-09-23 20:27:42 +0300

    update get params

--- src/app/models/form-search-params.ts
+++ src/app/models/form-search-params.ts
@@ -1,7 +1,12 @@
 export class FormSearchParams {
-    page?: number;
-    limit?: number;
-    search?: object;
-    sort?: object;
-    filter?: object;
+    page?: number = 0;
+    limit?: number = 15;
+    search?: string;
+    sort?: {
+      order: 'asc' | 'desc',
+      field: 'string'
+    };
+    filter?: {
+      [key: string]: string
+    };
 }
