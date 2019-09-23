7a9af0732811ddcdb3febd2f4d380da6313839ca
Vitalii Fedorets <vf@codeblue.ventures>
2019-09-23 23:59:55 +0300

    remove empty components

--- src/app/forms-dashboard/form-table.module.ts
+++ src/app/forms-dashboard/form-table.module.ts
@@ -2,10 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
-import { FormConstructorComponent } from '@app/form-constructor/form-constructor.component';
 import { FormTableRoutingModule } from './form-table-routing.module';
 import { FormTableComponent} from './form-table.component';
-import { OnlineFormComponent } from '@app/online-form/online-form.component';
 
 import { CdkTableModule } from '@angular/cdk/table';
 import { DataCollectionService } from './data-collection.service';
@@ -13,9 +11,7 @@ import { DataCollectionService } from './data-collection.service';
 
 @NgModule({
   declarations: [
-    FormConstructorComponent,
     FormTableComponent,
-    OnlineFormComponent,
   ],
   imports: [
     CommonModule,
