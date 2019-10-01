062f8eb99d87ceb7857011edc44e487acf4ebeed
Илья <i@devurai.com>
2019-10-01 05:43:26 +0300

    create widget component

---
+++ src/app/form-creator/workarea/fields/widget/widget.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PopupRef } from '@app/core/components/popup/popup.ref';
+import { Popup } from '@app/core/popup.service';
+
+@Component({
+  selector: 'sw-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss']
+})
+export class WidgetComponent implements OnInit {
+
+  private ref: PopupRef;
+
+  @ViewChild('holder', { static: false }) holder;
+  @ViewChild('widgetContent', { static: false }) widgetContent;
+
+  constructor(
+    private popup: Popup
+  ) { }
+
+  ngOnInit() {
+  }
+
+  widgetTogle(): void {
+    if (this.ref) {
+      this.ref.close();
+    } else {
+      this.openWidget();
+    }
+  }
+
+  openWidget() {
+    this.ref = this.popup.open({
+      origin: this.holder,
+      content: this.widgetContent,
+      location: 'center-down-left',
+      panelClass: 'widget-dropdown'
+    });
+    this.ref.afterClosed$.subscribe(result => {
+      this.ref = null;
+    })
+  }
+
+}
