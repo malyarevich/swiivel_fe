6db10a9587ad1b6d86e8e0704e9ec99e77bac6f9
Yerhabe <yeredhaber@gmail.com>
2019-10-10 14:17:40 +0300

    examples using knobs

--- src/app/modules/upload-review-form/upload-review-form-approval-bar/upload-review-form-approval-bar.stories.ts
+++ src/app/modules/upload-review-form/upload-review-form-approval-bar/upload-review-form-approval-bar.stories.ts
@@ -1,4 +1,5 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
 import { StatusLabelComponent } from '@shared/labels/status-label/status-label.component';
 import { IconButtonComponent } from '@shared/buttons/icon-button/icon-button.component';
 import { UploadReviewFormService  } from '@modules/upload-review-form/upload-review-form.service';
@@ -8,23 +9,30 @@ import {
 } from '@modules/upload-review-form/upload-review-form-approval-bar/upload-review-form-approval-bar.component';
 import { ButtonComponent } from '@shared/buttons/button/button.component';
 
-storiesOf('Upload Review Form', module)
-  .addDecorator(
-    moduleMetadata({
-      declarations: [UploadReviewFormApprovalBarComponent, StatusLabelComponent, IconButtonComponent, ButtonComponent],
-      providers: [UploadReviewFormService, HttpClient, HttpHandler]
-    })
-  )
-  .add('Upload Review Form Bar', () => ({
-      template: `<app-document-approval-bar [document]="document"></app-document-approval-bar>`,
-      props: {
-        document: {
-          entity_name: 'Name',
-          status: 'rejected',
-          submission_type: 'online_form',
-          person_name: 'SD name Jacobson',
-          isSelected: false,
-        },
+const stories = storiesOf('Upload Review Form', module);
+
+stories.addDecorator(withKnobs);
+stories.addDecorator(
+  moduleMetadata({
+    declarations: [UploadReviewFormApprovalBarComponent, StatusLabelComponent, IconButtonComponent, ButtonComponent],
+    providers: [UploadReviewFormService, HttpClient, HttpHandler]
+  })
+)
+
+const optionsStatus = [
+  'approved', 'assigned', 'need_review', 'need_further_review', 'not_started', 'rejected', 'unassigned',
+];
+
+stories.add('Upload Review Form Bar', () => ({
+    template: `<app-document-approval-bar [document]="document"></app-document-approval-bar>`,
+    props: {
+      document: {
+        entity_name: 'Name',
+        status: select('Status', optionsStatus, 'rejected'),
+        submission_type: 'online_form',
+        person_name: text('person_name', 'Ari Jacobson'),
+        isSelected: false,
       },
-    })
-  );
+    },
+  })
+);
