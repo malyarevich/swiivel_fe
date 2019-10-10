d21175114a43e9247e28c50b34c96688a654457b
Илья <i@devurai.com>
2019-10-10 13:40:25 +0300

    form builder size fix

--- src/app/modules/organization/components/creator-header/creator-header.component.ts
+++ src/app/modules/organization/components/creator-header/creator-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-creator-header',
@@ -11,15 +12,9 @@ export class CreatorHeaderComponent implements OnInit {
   constructor(
     private route: ActivatedRoute
   ) {
-    this.route.params.subscribe(params => {
-      console.log(params) //log the entire params object
-      console.log('ID Parametr', params['id']) //log the value of id
-    });
   }
 
   ngOnInit() {
-    console.log('ROUTE', this.route);
-    console.log('snapppp', this.route.snapshot.paramMap.get("id"));
   }
 
 }
