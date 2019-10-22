0e89dba76a94f3175dce6c40ab14fe1a356760a6
Artem Kuzvesov <arktuz@gmail.com>
2019-10-22 13:15:07 +0500

    upload rivew update

--- src/app/modules/upload-review-form/upload-review-form.module.ts
+++ src/app/modules/upload-review-form/upload-review-form.module.ts
@@ -15,16 +15,18 @@ import {
   UploadReviewFormDocumentSettingsComponent
 } from '@modules/upload-review-form/upload-review-form-document-settings/upload-review-form-document.component-settings';
 import { FormUploadFooterComponent } from '@modules/upload-review-form/upload-review-form-footer/upload-review-form-footer.component';
-import { UploadFormReviewItemComponent } from '@modules/upload-review-form/upload-review-form-item/upload-review-form-item.component';
+// import { OnlineFormComponent } from '@modules/online-form/online-form.component';
 import { SharedModule } from '@shared/shared.module';
 import { SharedRedComponentsModule } from '@app/shared/components/sharedRedComponents.module';
 import { UploadReviewFormRoutingModule } from './upload-review-form-routing.module';
 import { UploadReviewFormComponent } from './upload-review-form.component';
 import { UploadReviewFormService } from '@modules/upload-review-form/upload-review-form.service';
+import { UploadFormReviewItemComponent } from '@modules/upload-review-form/upload-review-form-item/upload-review-form-item.component';
 
 @NgModule({
   declarations: [
     FormUploadFooterComponent,
+    // OnlineFormComponent,
     UploadFormReviewItemComponent,
     UploadReviewFormApprovalBarComponent,
     UploadReviewFormComponent,
