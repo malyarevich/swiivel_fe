6d08f9fae03f9e947f34b04ec7d1b3a693daece8
Илья <i@devurai.com>
2019-10-24 12:49:41 +0300

    preview  component

--- src/app/modules/form-send/preview/preview.component.ts
+++ src/app/modules/form-send/preview/preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-preview',
@@ -8,10 +9,30 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class PreviewComponent implements OnInit {
   id: string;
+  formType: string = 'generic';
+  filter: FormControl = new FormControl('');
+  buttonOptions = [
+    {
+      label: 'Generic Form',
+      value: 'generic'
+    },
+    {
+      label: 'Account Form',
+      value: 'account'
+    }
+  ]
 
   constructor(
     private route: ActivatedRoute
-  ) { }
+  ) {
+    this.filter.valueChanges.subscribe((filterValue) => {
+      if (filterValue && filterValue.length > 0) {
+        // this.dataSource.filter(filterValue.toLowerCase())
+      } else {
+        // this.dataSource.filter('');
+      }
+    });
+  }
 
   ngOnInit() {
     this.route.parent.params.subscribe((params: Params) => {
