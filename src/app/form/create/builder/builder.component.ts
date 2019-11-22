ee34b185ed47bd6375c18cf97db8a8700f873836
Илья <i@devurai.com>
2019-11-22 05:38:01 +0200

    fix expanded button on sidebar

--- src/app/form/create/builder/builder.component.ts
+++ src/app/form/create/builder/builder.component.ts
@@ -148,6 +148,7 @@ export class BuilderComponent implements OnInit, OnDestroy {
       showControl.setValue(false);
       this.expandedSection = null;
     }
+    this.expanded = true;
     this.cdr.markForCheck()
   }
 
