a321a2690b6265ea4654a544955205da1f80841b
Paul Malyarevich <p@codeblue.ventures>
2019-11-28 15:14:35 +0000

    Form send trans

--- src/app/form-send/form-send.module.ts
+++ src/app/form-send/form-send.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormSendComponent } from './form-send.component';
-import { FormSendRoutingModule } from './form-send-routing.module';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
+import { FormSendRoutingModule } from './form-send-routing.module';
+import { FormSendComponent } from './form-send.component';
+import { FormSendService } from './form-send.service';
 import { SendPreviewComponent } from './send-preview/send-preview.component';
 import { SendReleaseComponent } from './send-release/send-release.component';
-import { FormSendService } from './form-send.service';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataCollectionService } from '@app/forms-dashboard/data-collection.service';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { PreviewFormComponent } from './preview-form/preview-form.component';
 import { OnlineFormModule } from '@app/online-form/online-form.module';
 import { PreviewFormPdfModule } from '@app/preview-form-pdf/preview-form-pdf.module';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
@@ -22,8 +21,7 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   declarations: [
     FormSendComponent,
     SendPreviewComponent,
-    SendReleaseComponent,
-    PreviewFormComponent
+    SendReleaseComponent
   ],
   imports: [
     CommonModule,
