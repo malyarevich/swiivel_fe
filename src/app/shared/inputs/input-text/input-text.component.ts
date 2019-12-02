d25e6d999ed87924d40f8a010de2e4c1f75f2e42
Vitalii <vf@codeblue.ventures>
2019-12-02 11:31:08 +0000

    add text input and button types inputs

--- src/app/shared/inputs/input-text/input-text.component.ts
+++ src/app/shared/inputs/input-text/input-text.component.ts
@@ -21,31 +21,35 @@ import { ControlValueAccessor, NgControl } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InputTextComponent implements ControlValueAccessor, OnInit {
+  public _type = 'text';
+  public _mode = 'text';
+  public _style = 'button';
   @Input() autofocus: boolean;
   @Input() mask: string;
   @Input() autocomplete: string;
-  public _type = 'text';
-  public _style = 'button';
-  @ViewChild('input', { static: true }) input: ElementRef;
   @Input() set type(inputType: string) {
     this._type = inputType;
+    if (this._type === 'email') this._mode = 'email';
+    else if (this._type === 'numeric') this._mode = 'numeric';
+    else if (this._type === 'tel') this._mode = 'tel';
+    else this._mode = 'text';
+  }
+  @Input() set mode(inputMode: string) {
+    this._mode = inputMode;
   }
   @Input() set style(styleType: string) {
     this._style = styleType;
   }
-  @Input() set readonly(readOnly: boolean) {
-    if (readOnly) {
-      this.control.control.disable();
-    } else {
-      this.control.control.enable();
-    }
-  }
+  @Input() disabled: boolean = null;
+  @Input() readonly: boolean = null;
   @Input() isSearch = false;
   @Input() isClearable = false;
   @Input() trimStart: boolean;
   @Output('blur') onBlur = new EventEmitter<any>();
   @Output('focus') onFocus = new EventEmitter<any>();
-
+  
+  @ViewChild('input', { static: true }) input: ElementRef;
+  
   writeValue = (value: string) => {};
   registerOnChange = (fn: any) => {};
   registerOnTouched = (fn: any) => {};
@@ -66,8 +70,8 @@ export class InputTextComponent implements ControlValueAccessor, OnInit {
     this.control.statusChanges.subscribe(() => {
       this.cdr.markForCheck();
     });
-    this.control.valueChanges.subscribe(v => {
-      if (this.trimStart) {
+    this.control.valueChanges.subscribe((v) => {
+      if (!!v && this.trimStart) {
         this.control.control.setValue(v.trimStart(), { emitEvent: false });
       }
     });
