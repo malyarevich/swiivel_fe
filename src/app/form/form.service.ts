cd752b529bb3e2f7890a4b07c97a594efb098057
Vitalii Fedorets <vf@codeblue.ventures>
2019-12-06 15:16:43 +0200

    fix widget, fix custom field form

--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -106,7 +106,8 @@ export class FormService {
   formsById = {};
   public fieldTypes = {
     schema: [],
-    mapped: []
+    mapped: [],
+    defaultOptions: []  
   };
   public stage$ = new BehaviorSubject(0);
   public dropLists = new Set();
