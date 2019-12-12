163c6ed634870c46898a2d83a13870ce1cdeaf43
Artem K <arktuz@yandex.ru>
2019-12-12 09:17:23 +0000

    add form-management access

--- src/app/form-management/form-management.module.ts
+++ src/app/form-management/form-management.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '@shared/shared.module';
 
 import { FormManagementRoutingModule } from '@app/form-management/form-management.routing.module';
 
+import { FormManagementAccessComponent } from '@app/form-management/form-management-access/form-management-access.component';
 import { FormManagementAnalyticsComponent } from '@app/form-management/form-management-analytics/form-management-analytics.component';
 import { FormManagementHeaderComponent } from '@app/form-management/form-management-header/form-management-header.component';
 import { FormManagementInfoComponent } from '@app/form-management/form-management-info/form-management-info.component';
@@ -38,6 +39,7 @@ import { FormManagementSubmissionsComponent } from './form-management-submission
     FormManagementSubmissionHistoryComponent,
     FormManagementLogsComponent,
     FormManagementSubmissionsComponent,
+    FormManagementAccessComponent,
   ],
   imports: [
     CommonModule,
