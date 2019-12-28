a0cf1fddd6a8fe69fa9b000365e691ad49b86d34
Paul Malyarevich <jonykeyt@gmail.com>
2019-12-29 00:57:03 +0200

    main component redesign to actual + added svg menu icons

--- src/app/shared/main.component.ts
+++ src/app/shared/main.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 export interface IMenuItems {
@@ -10,26 +10,47 @@ export interface IMenuItems {
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls : ['./main.component.scss'],
+  styleUrls: ['./main.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class MainComponent implements OnInit {
+  public isCollapsed = true;
+  public isLeftMenuOpened = false;
+
+  pathIconsFolder = 'assets/images/icons/';
   menuItems: IMenuItems[] = [
-    {route: '/dashboard', label: 'Dashboard', icon: 'fa-th'},
-    {route: '/bus', label: 'Bus', icon: 'fa-bus'},
-    {route: '/payer-accounts', label: 'Payer Accounts', icon: 'fa-wallet'},
-    {route: '/persons', label: 'Persons', icon: 'fa-user'},
-    {route: '/family', label: 'Family', icon: 'fa-cog'},
-    {route: '/period', label: 'Period', icon: 'fa-calendar'},
-    {route: '/forms-dashboard', label: 'Data Collection', icon: 'fa-file'},
+    { route: '/dashboard', label: 'Dashboard', icon: 'menu-dashboard' },
+    { route: '/persons', label: 'People', icon: 'menu-people' },
+    { route: '/payer-accounts', label: 'Financial', icon: 'menu-financial' },
+    { route: '/forms-dashboard', label: 'Data Collection', icon: 'menu-data-collection' },
+    { route: '/bus', label: 'Collections', icon: 'menu-collections' },
+    { route: '/family', label: 'Call Center', icon: 'menu-call-center' },
+    { route: '/period', label: 'Reporting', icon: 'menu-reporting' },
+    { route: '/communication', label: 'Communication', icon: 'menu-communication' },
+    { route: undefined, label: undefined, icon: undefined }, // splitter
+    { route: '/settings', label: 'Settings', icon: 'menu-settings' },
+    { route: '/logout', label: 'Log Out', icon: 'sign-out-alt-solid' },
   ];
-  constructor(private router: Router) {}
-  ngOnInit(): void {}
-  toggleMenu() {}
+  constructor(private router: Router) { }
+  ngOnInit(): void { }
+  toggleMenu() { }
+
+  isLogout(item: IMenuItems): boolean {
+    return item.route === '/logout';
+  }
+
   logOut() {
     window.localStorage.clear();
     window.sessionStorage.clear();
     this.router.navigate(['/login']);
+  }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  toggleLeftMenu() {
+    this.isLeftMenuOpened = !this.isLeftMenuOpened;
   }
 }
